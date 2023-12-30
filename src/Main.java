fcf2c585b1e5aadbe61de950a27379060ce6a65b
amir <am.sharafi@gmail.com>
2023-12-31 02:48:40 +0330

    Second commit (not fully checked) Doesn't come back to previous menu try at Main has a problem

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,11 @@ import Service.ClientsConsole;
 
 public class Main {
     public static void main(String[] args) {
-        ClientsConsole newConsole = new ClientsConsole();
+        try (ClientsConsole newConsole = new ClientsConsole()) {
             newConsole.run();
+        } catch (Exception e) {
+            System.out.println("Error occurred!");
+        }
     }
 }
 
-//try(ClientsConsole newConsole = new ClientsConsole()) {
-//        newConsole.run();
-//        } catch(Exception e) {
-//        System.out.println("From Exception Block");
-//        }
